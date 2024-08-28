a755c598b86a00cefe7e32f6cc14bc95d5608281
unknown <jcn.borges@gmail.com>
2024-08-27 21:48:46 -0300

    Subtraction, multiplication and division operations

--- rest-math/src/main/java/br/com/julio/MathController.java
+++ rest-math/src/main/java/br/com/julio/MathController.java
@@ -1,7 +1,5 @@
 package br.com.julio;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,20 +8,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
-	private final AtomicLong counter = new AtomicLong();
-	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}",
 			method = RequestMethod.GET)
 	public Double sum(
 			@PathVariable(value = "numberOne") String numberOne, 
-			@PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+			@PathVariable(value = "numberTwo") String numberTwo) throws NumberFormatException {		
 		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-			throw new Exception();
+			throw new NumberFormatException();
 		}
 		
 		return convertToDouble(numberOne) + convertToDouble(numberTwo);		
 	}
+	
+	@RequestMapping(value = "/sub/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double sub(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws NumberFormatException {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new NumberFormatException();
+		}
+		
+		return convertToDouble(numberOne) - convertToDouble(numberTwo);
+	}
 
+	@RequestMapping(value = "/mul/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double mul(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws NumberFormatException {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new NumberFormatException();
+		}
+		
+		return convertToDouble(numberOne) * convertToDouble(numberTwo);
+	}	
+
+	@RequestMapping(value = "/div/{numberOne}/{numberTwo}",
+			method = RequestMethod.GET)
+	public Double div(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "numberTwo") String numberTwo) throws NumberFormatException {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new NumberFormatException();
+		}
+		
+		return convertToDouble(numberOne) / convertToDouble(numberTwo);
+	}	
+	
 	private Double convertToDouble(String strNumber) {
 		if (strNumber == null) return 0D;
 		String number = strNumber.replaceAll(",", ".");
